09ef0736c8ba481be192f27a3858d9e14daf6bca
Rohit <rt23707@gmail.com>
2024-03-12 18:23:37 +0530

    scrapy amazon with pagination

--- python_scrapy/spiders/amazon.py
+++ python_scrapy/spiders/amazon.py
@@ -7,7 +7,15 @@ class AmazonSpider(scrapy.Spider):
     start_urls = [
         "https://www.amazon.in/s?k=shoes+for+men&crid=JAVU9OVPE4Y9&sprefix=shoes+for+men%2Caps%2C289&ref=nb_sb_ss_ts-doa-p_1_13"]
 
+
+
+
+    def __init__(self):
+        self.page_count = 0  # Initialize page count
+
     def parse(self, response):
+        self.page_count += 1
+
         products = response.xpath("//div[@data-component-type='s-search-result']")
 
         for product in products:
@@ -20,3 +28,7 @@ class AmazonSpider(scrapy.Spider):
                 "Price": price.strip() if price else None,
                 "Discount": discount.strip() if discount else None,
             }
+
+            next_page_url = response.xpath("//div/div/span/a[@class='s-pagination-item s-pagination-next s-pagination-button s-pagination-separator']/@href").get()
+            if next_page_url and self.page_count < 4:
+                yield response.follow(next_page_url, callback=self.parse)
